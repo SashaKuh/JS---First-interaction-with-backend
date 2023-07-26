4b4bdc87a25abfeba5a4539adde0fa5c52bfb8d9
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-07-26 15:26:21 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,57 +3,56 @@ import { Loading, Report } from 'notiflix';
 
 const elements = {
   breedSelect: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader')
+  catInfo: document.querySelector('.cat-info')
 };
 
 elements.breedSelect.classList.add('hidden');
 Loading.standard('Loading data, please wait...');
 
 const createMarkupCatInfo = (cat) => `
-<div class="container">
+  <div class="container">
     <div class="thumb-pic">
-        <img src="${cat.url}" alt="${cat.id}" height="350" />
+      <img src="${cat.url}" alt="${cat.id}" height="350" />
     </div>
     <div class="thumb">
-        <h1>${cat.breeds[0].name}</h1>
-        <p>${cat.breeds[0].description}</p>
-        <p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
+      <h1>${cat.breeds[0].name}</h1>
+      <p>${cat.breeds[0].description}</p>
+      <p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
     </div>
-</div>`;
+  </div>`;
 
 const handleBreedSelectChange = (evt) => {
   evt.preventDefault();
-  elements.breedSelect.classList.add('hidden'); 
-  elements.catInfo.innerHTML = ''; 
+  elements.catInfo.style.display = 'none'; 
   Loading.standard('Loading data, please wait...');
+
   const breedSelectId = elements.breedSelect.value;
 
   fetchCatByBreed(breedSelectId)
     .then(cat => {
       Loading.remove();
-      elements.catInfo.innerHTML = createMarkupCatInfo(cat); 
-      elements.breedSelect.classList.remove('hidden');
+      elements.catInfo.innerHTML = createMarkupCatInfo(cat);
+      elements.catInfo.style.display = 'block'; 
     })
     .catch(error => {
       console.error(error);
-      Report.failure('Oops! Something went wrong! ');
-      elements.breedSelect.classList.remove('hidden'); 
-    })
-    .finally(() => Loading.remove());
+      Report.failure('Oops! Something went wrong!');
+      Loading.remove();
+      elements.catInfo.style.display = 'block'; 
+    });
 };
 
 fetchBreeds()
   .then(data => {
-    console.log(data);
     const option = data.map(({ id, name }) => `<option value="${id}">${name}</option>`);
     elements.breedSelect.innerHTML = option;
     Loading.remove();
-    elements.breedSelect.classList.remove('hidden'); 
+    elements.breedSelect.style.display = 'block'; 
   })
   .catch(() => {
-    Report.failure('Oops! Something went wrong! ');
-    elements.breedSelect.classList.remove('hidden'); 
+    Report.failure('Oops! Something went wrong!');
+    Loading.remove();
+    elements.breedSelect.style.display = 'block'; 
   });
 
 elements.breedSelect.addEventListener('change', handleBreedSelectChange);
