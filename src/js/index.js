daf625f3c0b766f8ccd2c11cfb46e495e254dbef
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-07-26 13:42:12 +0300

    more fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,12 @@ import { Loading, Report } from 'notiflix';
 
 const elements = {
   breedSelect: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info')
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader')
 };
+
+elements.breedSelect.classList.add('hidden');
+Loading.standard('Loading data, please wait...');
 
 const createMarkupCatInfo = (cat) => `
 <div class="container">
@@ -13,25 +17,30 @@ const createMarkupCatInfo = (cat) => `
     </div>
     <div class="thumb">
         <h1>${cat.breeds[0].name}</h1>
-        <p><b>Description:</b> ${cat.breeds[0].description}</p>
+        <p>${cat.breeds[0].description}</p>
         <p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
     </div>
 </div>`;
 
 const handleBreedSelectChange = (evt) => {
   evt.preventDefault();
+  elements.breedSelect.classList.add('hidden'); 
+  elements.catInfo.innerHTML = ''; 
   Loading.standard('Loading data, please wait...');
   const breedSelectId = elements.breedSelect.value;
 
   fetchCatByBreed(breedSelectId)
     .then(cat => {
       Loading.remove();
-      elements.catInfo.innerHTML = createMarkupCatInfo(cat);
+      elements.catInfo.innerHTML = createMarkupCatInfo(cat); 
+      elements.breedSelect.classList.remove('hidden');
     })
     .catch(error => {
       console.error(error);
       Report.failure('Oops! Something went wrong! ');
-    });
+      elements.breedSelect.classList.remove('hidden'); 
+    })
+    .finally(() => Loading.remove());
 };
 
 fetchBreeds()
@@ -40,9 +49,11 @@ fetchBreeds()
     const option = data.map(({ id, name }) => `<option value="${id}">${name}</option>`);
     elements.breedSelect.innerHTML = option;
     Loading.remove();
+    elements.breedSelect.classList.remove('hidden'); 
   })
   .catch(() => {
     Report.failure('Oops! Something went wrong! ');
+    elements.breedSelect.classList.remove('hidden'); 
   });
 
 elements.breedSelect.addEventListener('change', handleBreedSelectChange);
